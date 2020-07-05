33c035912ba3a27429ffb96c567d1de28050c4bb
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-05 13:55:55 +0300

    import DevConfig from config.py and setup configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from .config import DevConfig
 
 # Initializing application
 
 app = Flask(__name__)
+
+# Setting up confinguration 
+app.config.from_object(DevConfig)
 
 from app import views
