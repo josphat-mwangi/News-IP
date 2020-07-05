709c92d881040a6ffde1aad0b1d8fc9af71fd1c9
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-05 14:38:52 +0300

    add route to article and source

--- app/views.py
+++ app/views.py
@@ -12,3 +12,19 @@ def index():
     '''
     title = 'Home - Welcome to The best News and Article website Online'
     return render_template('index.html', title=title)
+
+
+@app.route('/source')
+def source(source_id):
+    '''
+    View source page function that returns source details page and its data
+    '''
+    return render_template('source.html')
+
+
+@app.route('/article')
+def source(source_id):
+    '''
+    View article page function that returns article details page and its data
+    '''
+    return render_template('article.html')
