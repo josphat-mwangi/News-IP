2408b55ddb9248d109e6cc3e1293242dee5840c8
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-05 14:08:53 +0300

    add title to index.hmtl

--- app/views.py
+++ app/views.py
@@ -2,10 +2,13 @@ from flask import render_template
 from app import app
 
 # views
+
+
 @app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data 
 
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to The best News and Article website Online'
+    return render_template('index.html', title=title)
