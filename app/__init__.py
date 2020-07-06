e41b6340b229175a537f6605b00a4fbefd0ab677
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-07-06 12:18:39 +0300

    create main folder and make it a blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
+
+bootstrap = Bootstrap()
 
 # Initializing application
 
-app = Flask(__name__,instance_relative_config = True)
 
-# Setting up confinguration 
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-from app import views
-from app import error
+    app = Flask(__name__)
+    # creating  the app configurations
+    app.config.from_object(config_options[config_name])
+
+
+# Intializing flask extensions
+bootstrap.init_app(app)
+
+# Registering the blueprint
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+return app
